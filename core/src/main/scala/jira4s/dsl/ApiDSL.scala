a0831ea204df03cf57b9fa19165bdb78eedb4db5
Shoma Nishitateno <shoma.nishitateno@nulab.com>
2020-03-08 11:29:34 +0900

    remove: once these remove for solving compile errors

--- core/src/main/scala/jira4s/dsl/ApiDSL.scala
+++
@@ -1,8 +0,0 @@
-package jira4s.dsl
-
-import cats.free.Free
-
-object ApiDSL {
-  type ApiADT[A] = HttpADT[A]
-  type ApiProgram[A] = Free[ApiADT, A]
-}
--- core/src/main/scala/jira4s/exceptions/JiraApiException.scala
+++
@@ -1,7 +0,0 @@
-package jira4s.exceptions
-
-import jira4s.dsl.HttpError
-
-case class JiraApiException(error: HttpError) extends RuntimeException {
-  override def getMessage: String = s"Request failed with error $error"
-}
