cf439f368a490571ef54e167e156a6e5043179f8
yudaiogawa <19630931+yudaiogawa@users.noreply.github.com>
2020-03-24 14:50:12 +0900

    #1 add: json definition related to version

--- core/src/main/scala/tech/ignission/jira4s/v3/formatters/SprayJsonFormats.scala
+++ core/src/main/scala/tech/ignission/jira4s/v3/formatters/SprayJsonFormats.scala
@@ -2,9 +2,24 @@ package tech.ignission.jira4s.v3.formatters
 
 import spray.json._
 import tech.ignission.jira4s.v3.ApiErrors
-import tech.ignission.jira4s.v3.datas.User
+import tech.ignission.jira4s.v3.datas._
 
 object SprayJsonFormats extends DefaultJsonProtocol {
+
+  // Foundation types
+  implicit object CalendarDateFormat extends RootJsonFormat[CalendarDate] {
+    def write(c: CalendarDate) =
+      JsString("")
+
+    def read(value: JsValue) =
+      value match {
+        case JsString(_) => CalendarDate()
+        case _ => deserializationError("String expected")
+      }
+  }
+
+  // Version
+  implicit val versionFormat: JsonFormat[Version] = jsonFormat6(Version)
 
   // User
   implicit val userFormat: JsonFormat[User] = jsonFormat4(User)
