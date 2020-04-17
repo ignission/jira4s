88afbaeea82881f28fcb5b4e428c3fd0a65597e9
Shoma Nishitateno <shoma416@gmail.com>
2020-04-18 01:27:08 +0900

    update: api parameter needs project id or key

--- jira4s-core/src/main/scala/tech/ignission/jira4s/v3/formatters/SprayJsonFormats.scala
+++ jira4s-core/src/main/scala/tech/ignission/jira4s/v3/formatters/SprayJsonFormats.scala
@@ -7,6 +7,41 @@ import tech.ignission.jira4s.v3.datas._
 object SprayJsonFormats extends DefaultJsonProtocol {
 
   // Foundation types
+  // class IdFormat[A]() extends RootJsonFormat[Id[A]] {
+  //   override def read(json: JsValue): Id[A] =
+  //     json match {
+  //       case JsNumber(idVal) => Id(idVal.toDouble)
+  //       case JsString(idVal) => Id(idVal.toDouble)
+  //       case _ =>
+  //         throw DeserializationException(s"Expected a js number got ${json.prettyPrint}")
+  //     }
+
+  //   override def write(obj: Id[A]): JsValue = JsNumber(obj.value)
+  // }
+
+  // class KeyFormat[A]() extends RootJsonFormat[Key[A]] {
+  //   override def read(json: JsValue): Key[A] =
+  //     json match {
+  //       case JsString(keyVal) => Key(keyVal)
+  //       case _ =>
+  //         throw DeserializationException(s"Expected a js string got ${json.prettyPrint}")
+  //     }
+
+  //   override def write(obj: Key[A]): JsValue = JsString(obj.value)
+  // }
+
+  // class IdOrKeyFormat[A]() extends RootJsonFormat[IdOrKeyParam[A]] {
+  //   override def read(json: JsValue): IdOrKeyParam[A] =
+  //     json match {
+  //       case JsNumber(idVal) => IdParam[A](Id[A](idVal.toInt))
+  //       case JsString(keyVal) => KeyParam[A](Key[A](keyVal))
+  //       case _ =>
+  //         deserializationError(s"Expected a string or a number for IdOrKeyParam got ${json.prettyPrint}")
+  //     }
+
+  //   override def write(idOrKey: IdOrKeyParam[A]): JsValue = JsString(idOrKey.toString)
+  // }
+
   implicit object CalendarDateFormat extends RootJsonFormat[CalendarDate] {
     def write(c: CalendarDate) =
       JsString("")
