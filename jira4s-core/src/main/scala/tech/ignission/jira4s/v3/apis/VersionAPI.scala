e766622e4cf9bdcc4e1ee1d94d5d03e84da4c49a
Shoma Nishitateno <shoma416@gmail.com>
2020-04-19 00:24:01 +0900

    #8 fix: version id contains decimal

--- jira4s-core/src/main/scala/tech/ignission/jira4s/v3/apis/VersionAPI.scala
+++ jira4s-core/src/main/scala/tech/ignission/jira4s/v3/apis/VersionAPI.scala
@@ -4,6 +4,7 @@ import tech.ignission.jira4s.v3.Credentials
 import tech.ignission.jira4s.v3.datas.{Id, IdOrKeyParam, Project, Version}
 import tech.ignission.jira4s.v3.dsl.BacklogHttpDsl.Response
 import tech.ignission.jira4s.v3.dsl.{HttpDSL, HttpQuery}
+import tech.ignission.jira4s.v3.datas.IdParam
 
 class VersionAPI[F[_]](baseUrl: String, credentials: Credentials)(implicit httpDSL: HttpDSL[F]) {
   import tech.ignission.jira4s.v3.formatters.SprayJsonFormats._
@@ -19,10 +20,10 @@ class VersionAPI[F[_]](baseUrl: String, credentials: Credentials)(implicit httpD
       )
     )
 
-  def get(id: Id[Version]): F[Response[Version]] =
+  def get(id: IdParam[Version]): F[Response[Version]] =
     httpDSL.get[Version](
       HttpQuery(
-        path = s"$resource/version/$id",
+        path = s"$resource/version/${id.toString}",
         credentials = credentials,
         baseUrl = baseUrl
       )
--- jira4s-core/src/main/scala/tech/ignission/jira4s/v3/datas/Id.scala
+++ jira4s-core/src/main/scala/tech/ignission/jira4s/v3/datas/Id.scala
@@ -1,13 +1,15 @@
 package tech.ignission.jira4s.v3.datas
 
+// Domain
 case class Id[A](value: Double) extends AnyVal
 case class Key[A](value: String) extends AnyVal
 
+// Request
 sealed trait IdOrKeyParam[A] {
   override def toString: String
 }
 case class IdParam[A](id: Id[A]) extends IdOrKeyParam[A] {
-  override def toString: String = id.value.toString
+  override def toString: String = id.value.toInt.toString
 }
 case class KeyParam[A](key: Key[A]) extends IdOrKeyParam[A] {
   override def toString: String = key.value
@@ -16,4 +18,9 @@ case class KeyParam[A](key: Key[A]) extends IdOrKeyParam[A] {
 object IdOrKeyParam {
   def projectKey(key: Key[Project]): IdOrKeyParam[Project] =
     KeyParam[Project](key)
+}
+
+object IdParam {
+  def versionId(id: Id[Version]): IdParam[Version] = 
+    IdParam[Version](id)
 }
