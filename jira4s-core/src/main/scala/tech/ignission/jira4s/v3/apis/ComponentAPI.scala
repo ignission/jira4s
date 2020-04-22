cd6a87e28271548083c6a4b90e0cb87f7b8d758a
yudaiogawa <19630931+yudaiogawa@users.noreply.github.com>
2020-04-22 11:58:02 +0900

    #11 add: api to get project component

--- jira4s-core/src/main/scala/tech/ignission/jira4s/v3/apis/ComponentAPI.scala
+++ jira4s-core/src/main/scala/tech/ignission/jira4s/v3/apis/ComponentAPI.scala
@@ -1,10 +1,9 @@
 package tech.ignission.jira4s.v3.apis
 
 import tech.ignission.jira4s.v3.Credentials
-import tech.ignission.jira4s.v3.datas.{Id, IdOrKeyParam, Project, Version}
+import tech.ignission.jira4s.v3.datas.{IdParam, IdOrKeyParam, Project, Component}
 import tech.ignission.jira4s.v3.dsl.BacklogHttpDsl.Response
 import tech.ignission.jira4s.v3.dsl.{HttpDSL, HttpQuery}
-import tech.ignission.jira4s.v3.datas.Component
 
 class ComponentAPI[F[_]](baseUrl: String, credentials: Credentials)(implicit httpDSL: HttpDSL[F]) {
   import tech.ignission.jira4s.v3.formatters.SprayJsonFormats._
@@ -17,6 +16,15 @@ class ComponentAPI[F[_]](baseUrl: String, credentials: Credentials)(implicit htt
         path = s"$resource/project/$projectIdOrKey/components",
         credentials = credentials,
         baseUrl = baseUrl
+      )
+    )
+
+  def get(id: IdParam[Component]): F[Response[Component]] =
+    httpDSL.get[Component](
+      HttpQuery(
+      path = s"$resource/component/${id.toString}",
+      credentials = credentials,
+      baseUrl = baseUrl
       )
     )
 }
